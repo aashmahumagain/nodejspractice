7607042fe597e92e0986a75861a10168bafe043e
Aashma Humagain <aashma_humagain@axiossoftwork.com>
2022-12-29 19:03:48 +0545

    changes

--- controller/public_controller.js
+++ controller/public_controller.js
@@ -1,15 +1,19 @@
 exports.getFirstPage = (req, res) => {
-    res.render("index");
-}
+  res.render("index");
+};
 
 exports.getAboutPage = (req, res) => {
-    res.json("Hello about page!");
-}
+  res.json("Hello about page!");
+};
 
 exports.getContactPage = (req, res) => {
-    res.json("Hello contact page!");
-}
+  res.json("Hello contact page!");
+};
 
 exports.getLoginController = (req, res) => {
-    res.render("users/index", { title: "Login" });
-}
+  res.render("users/index", { title: "Login" });
+};
+
+exports.getRegisterController = (req, res) => {
+  res.render("users/register", { title: "Register" });
+};
--- routes/public_routes.js
+++ routes/public_routes.js
@@ -2,15 +2,17 @@ const express = require("express");
 const appRouter = express.Router();
 
 const {
-    getFirstPage,
-    getAboutPage,
-    getContactPage,
-    getLoginController
+  getFirstPage,
+  getAboutPage,
+  getContactPage,
+  getLoginController,
+  getRegisterController,
 } = require("../controller/public_controller");
 
 appRouter.get("/", getFirstPage);
 appRouter.get("/about", getAboutPage);
 appRouter.get("/contat", getContactPage);
 appRouter.get("/login", getLoginController);
+appRouter.get("/register", getRegisterController);
 
 module.exports = appRouter;
